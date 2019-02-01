5ecaad9202565b2ee19a23322b8a373607aad557
amoozegar <amoozegar96@gmail.com>
2019-02-01 18:24:25 +0330

    fix projects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $('.carousel').carousel({
     wrap: true,
     touch: true,
-    keyboard:true
+    keyboard: true,
+    interval: 3000,
+    next: false
+});
+$(window).scroll(function () {
+    var fixmenu = $(window).scrollTop();
+    if (fixmenu <= $(".bottom-header").offset().top - 20) {
+        $(".header").removeClass("FixMenuLarge")
+    }
+    if (fixmenu >= $(".bottom-header").offset().top - 20) {
+        $(".header").addClass("FixMenuLarge")
+    }
 });
