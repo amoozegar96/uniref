47221423ecb5bcc0b8df2e5c5ed9d8516c451f0e
amoozegar <amoozegar96@gmail.com>
2019-02-02 18:42:38 +0330

    fix projects

--- js/main.js
+++ js/main.js
@@ -8,9 +8,30 @@ $('.carousel').carousel({
 $(window).scroll(function () {
     var fixmenu = $(window).scrollTop();
     if (fixmenu <= $(".bottom-header").offset().top - 20) {
-        $(".header").removeClass("FixMenuLarge")
+        $(".header").removeClass("fix-menu-large")
     }
     if (fixmenu >= $(".bottom-header").offset().top - 20) {
-        $(".header").addClass("FixMenuLarge")
+        $(".header").addClass("fix-menu-large")
     }
 });
+
+$(".icon-menu-small").click(function (e) {
+    $(".menu-items").css({right: 0});
+    $(".black-page").fadeIn();
+    $("html").css({overflow: "hidden"});
+    e.stopPropagation();
+    e.preventDefault();
+});
+
+$(".black-page , .close-menu i").click(function () {
+    $(".menu-items").css({right: -300});
+    $(".black-page").fadeOut();
+    $("html").removeAttr("style");
+});
+
+$(".menu-items ul li div i.fas.fa-plus").click(function () {
+    $(this).parents("ul li").find("ul.menu-down").stop().slideToggle();
+    $(this).toggleClass("fa-plus");
+    $(this).toggleClass("fa-minus");
+});
+
