12133827dba46511bf79b846083f0f345653d1d2
Mr.black <ali.programmer78@gmail.com>
2019-03-03 20:01:10 +0330

    add sidebar,fix bug chart in index page add new image.

--- js/main.js
+++ js/main.js
@@ -65,7 +65,210 @@ $(".gallery-sidebar-content li").click(function () {
 // chart
 //==================
 
-$('[data-chart="3d-stacked"]').each(function(){
+function formatNumbers(value) {
+    var persian = {
+        0: '۰',
+        1: '۱',
+        2: '۲',
+        3: '۳',
+        4: '۴',
+        5: '۵',
+        6: '۶',
+        7: '۷',
+        8: '۸',
+        9: '۹'
+    };
+    var res = String(value);
+    var list = res.match(/[0-9]/g);
+    if (list != null && list.length != 0) {
+        for (var i = 0; i < list.length; i++)
+            res = res.replace(list[i], persian[list[i]]);
+    }
+    return res;
+}
+
+function formatBalloon(item) {
+    var res = formatNumbers(item.category) + ': <b>' + formatNumbers(item.values.value) + '</b>'
+    return res;
+}
+
+
+// Create chart instance
+if ($("#gauge-bands").length) {
+
+    function am4themes_myTheme(target) {
+        if (target instanceof am4core.ColorSet) {
+            target.list = [
+                am4core.color("#29b6b6"),
+                am4core.color("#875692"),
+                am4core.color("#f38600"),
+                am4core.color("#3b85ca"),
+            ];
+        }
+    }
+
+    /* Apply it */
+    am4core.useTheme(am4themes_myTheme);
+
+// Themes begin
+// Themes end
+    var chart = am4core.create("gauge-bands", am4charts.RadarChart);
+// chart.align = 'left';
+// chart.rtl = true;
+// Add data
+    chart.data = [{
+        "category": "(دانشگاه پیام  نور (۴ درصد",
+        "value": 80,
+        "full": 100,
+    }, {
+        "category": "(موسسه غیر انتفاهی (۵ درصد",
+        "value": 35,
+        "full": 100,
+    }, {
+        "category": "(دانشگاه دولتی (۵۳ درصد",
+        "value": 92,
+        "full": 100,
+    }, {
+        "category": "(دانشگاه آزاد (33درصد",
+        "value": 68,
+        "full": 100,
+    }];
+
+// Make chart not full circle
+    chart.startAngle = -90;
+    chart.endAngle = 180;
+    chart.innerRadius = am4core.percent(20);
+
+// Set number format
+    chart.numberFormatter.numberFormat = "#.#'%'";
+
+// Create axes
+    var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.align = 'right';
+    categoryAxis.ex = 100;
+    categoryAxis.dataFields.category = "category";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.grid.template.strokeOpacity = 0;
+    categoryAxis.renderer.labels.template.horizontalCenter = "right";
+    categoryAxis.renderer.labels.template.fontWeight = 500;
+    categoryAxis.renderer.labels.template.adapter.add("fill", function (fill, target) {
+        return (target.dataItem.index >= 0) ? chart.colors.getIndex(target.dataItem.index) : fill;
+    });
+    categoryAxis.renderer.minGridDistance = 10;
+
+    var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.grid.template.strokeOpacity = 0;
+    valueAxis.min = 0;
+    valueAxis.max = 100;
+    valueAxis.strictMinMax = true;
+
+// Create series
+    var series1 = chart.series.push(new am4charts.RadarColumnSeries());
+    series1.dataFields.valueX = "full";
+    series1.dataFields.categoryY = "category";
+    series1.clustered = false;
+    series1.columns.template.fill = new am4core.InterfaceColorSet().getFor("alternativeBackground");
+    series1.columns.template.fillOpacity = 0.08;
+    series1.columns.template.cornerRadiusTopLeft = 20;
+    series1.columns.template.strokeWidth = 0;
+    series1.columns.template.radarColumn.cornerRadius = 20;
+    var series2 = chart.series.push(new am4charts.RadarColumnSeries());
+    series2.dataFields.valueX = "value";
+    series2.dataFields.categoryY = "category";
+    series2.clustered = false;
+    series2.columns.template.strokeWidth = 0;
+    series2.columns.template.radarColumn.cornerRadius = 20;
+    series2.columns.template.adapter.add("fill", function (fill, target) {
+        return chart.colors.getIndex(target.dataItem.index);
+    });
+
+// Add cursor
+// chart.cursor = new am4charts.RadarCursor();
+}
+
+
+function am4themes_myThemee(target) {
+    if (target instanceof am4core.ColorSet) {
+        target.list = [
+            am4core.color("#85c5e3")
+        ];
+    }
+}
+
+/* Apply it */
+
+if ($("#gauge-bands2").length) {
+    am4core.useTheme(am4themes_myThemee);
+// Themes end
+
+// Create chart instance
+    var chart2 = am4core.create("gauge-bands2", am4charts.XYChart);
+
+// Add data
+    chart2.data = [{
+        "country": "1385",
+        "visits": 900
+    }, {
+        "country": "1386",
+        "visits": 800
+    }, {
+        "country": "1387",
+        "visits": 700
+    }, {
+        "country": "1388",
+        "visits": 600
+    }, {
+        "country": "1389",
+        "visits": 500
+    }, {
+        "country": "1390",
+        "visits": 400
+    }, {
+        "country": "1391",
+        "visits": 300
+    }, {
+        "country": "1392",
+        "visits": 200
+    }, {
+        "country": "1393",
+        "visits": 50
+    }, {
+        "country": "1394",
+        "visits": 40
+    }, {
+        "country": "1395",
+        "visits": 30
+    }, {
+        "country": "1396",
+        "visits": 29
+    }, {
+        "country": "1397",
+        "visits": 15
+    }];
+
+// Create axes
+
+    var categoryAxis2 = chart2.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis2.dataFields.category = "country";
+    categoryAxis2.renderer.grid.template.location = 0;
+    categoryAxis2.renderer.minGridDistance = 30;
+
+
+    var valueAxis2 = chart2.yAxes.push(new am4charts.ValueAxis());
+
+// Create series
+    var series2 = chart2.series.push(new am4charts.ColumnSeries());
+    series2.dataFields.valueY = "visits";
+    series2.dataFields.categoryX = "country";
+    series2.name = "Visits";
+    series2.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series2.columns.template.fillOpacity = .8;
+
+    var columnTemplate2 = series2.columns.template;
+    columnTemplate2.strokeWidth = 2;
+    columnTemplate2.strokeOpacity = 1;
+}
+$('[data-chart="3d-stacked"]').each(function () {
     // Themes begin
     am4core.useTheme(am4themes_animated);
 // Themes end
@@ -119,7 +322,7 @@ $('[data-chart="3d-stacked"]').each(function(){
     categoryAxis.renderer.minGridDistance = 30;
 
     var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.renderer.labels.template.adapter.add("text", function(text) {
+    valueAxis.renderer.labels.template.adapter.add("text", function (text) {
         return text + "%";
     });
 
@@ -139,11 +342,10 @@ $('[data-chart="3d-stacked"]').each(function(){
     series2.clustered = false;
     series2.columns.template.tooltipText = "نماد رشد در سال {category} (2017): [bold]{valueY}[/]";
 });
-$('[data-chart="chart"]').each(function(){
+$('[data-chart="chart"]').each(function () {
     // Themes begin
     am4core.useTheme(am4themes_animated);
 // Themes end
-    console.log($(this).attr("id"));
 // Create chart instance
     var chart = am4core.create($(this).attr("id"), am4charts.XYChart);
 
@@ -280,10 +482,9 @@ $('[data-chart="chart"]').each(function(){
     };
 
     createTrendLine([
-        { "date": "2012-01-02", "value": 10 },
-        { "date": "2012-01-19", "value": 17 }
+        {"date": "2012-01-02", "value": 10},
+        {"date": "2012-01-19", "value": 17}
     ]);
-
 
 
 // Initial zoom once chart is ready
@@ -291,7 +492,7 @@ $('[data-chart="chart"]').each(function(){
 //         series.xAxis.zoomToDates(new Date(2012, 0, 2), new Date(2012, 0, 13));
 //     });
 });
-$('[data-chart="simple-column-chart"]').each(function(){
+$('[data-chart="simple-column-chart"]').each(function () {
     // Themes begin
     am4core.useTheme(am4themes_animated);
 // Themes end
@@ -348,7 +549,7 @@ $('[data-chart="simple-column-chart"]').each(function(){
     categoryAxis.renderer.grid.template.location = 0;
     categoryAxis.renderer.minGridDistance = 30;
 
-    categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
+    categoryAxis.renderer.labels.template.adapter.add("dy", function (dy, target) {
         if (target.dataItem && target.dataItem.index & 2 == 2) {
             return dy + 25;
         }
@@ -370,7 +571,7 @@ $('[data-chart="simple-column-chart"]').each(function(){
     columnTemplate.strokeOpacity = 1;
 });
 
-$('[data-chart="column-and-line-mix"]').each(function(){
+$('[data-chart="column-and-line-mix"]').each(function () {
     // Themes begin
     am4core.useTheme(am4themes_animated);
 // Themes end
@@ -379,7 +580,7 @@ $('[data-chart="column-and-line-mix"]').each(function(){
     var chart = am4core.create($(this).attr("id"), am4charts.XYChart);
 
 // Data for both series
-    var data = [ {
+    var data = [{
         "year": "2009",
         "income": 23.5,
         "expenses": 21.1
@@ -408,7 +609,7 @@ $('[data-chart="column-and-line-mix"]').each(function(){
         "columnDash": "5,5",
         "fillOpacity": 0.2,
         "additional": "(projection)"
-    } ];
+    }];
 
     /* Create axes */
     var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
@@ -452,7 +653,7 @@ $('[data-chart="column-and-line-mix"]').each(function(){
     chart.data = data;
 });
 
-$('[data-chart="variable-radius-pie-chart"]').each(function(){
+$('[data-chart="variable-radius-pie-chart"]').each(function () {
     // Themes begin
     am4core.useTheme(am4themes_animated);
 // Themes end
@@ -498,7 +699,83 @@ $('[data-chart="variable-radius-pie-chart"]').each(function(){
 });
 
 
-$('[data-chart="radia-line-graph"]').each(function(){
+$('[data-chart="radial-line-graph"]').each(function () {
+// Themes begin
+    am4core.useTheme(am4themes_animated);
+// Themes end
+
+    /* Create chart instance */
+    var chart = am4core.create($(this).attr("id"), am4charts.RadarChart);
+
+    /* Add data */
+    chart.data = [{
+        "country": "توسعه پایدار",
+        "litres": 501
+    }, {
+        "country": "ایران",
+        "litres": 301
+    }, {
+        "country": "کارآفرینی",
+        "litres": 266
+    }, {
+        "country": "آلودگی هوا",
+        "litres": 1009
+    }, {
+        "country": "گردشگری",
+        "litres": 139
+    }, {
+        "country": "توسعه",
+        "litres": 336
+    }, {
+        "country": "اقتصاد مقاومتی",
+        "litres": 290
+    }, {
+        "country": "معماری",
+        "litres": 325
+    }, {
+        "country": "عملکرد",
+        "litres": 40
+    }, {
+        "country": "مدیریت دانش",
+        "litres": 39
+    }, {
+        "country": "سنجش از دور",
+        "litres": 900
+    }, {
+        "country": "خسشکسالی",
+        "litres": 253
+    }, {
+        "country": "تهران",
+        "litres": 152
+    }, {
+        "country": "ارزیابی عملکرد",
+        "litres": 16
+    }, {
+        "country": "gis",
+        "litres": 465
+    }, {
+        "country": "مدیریت شهری",
+        "litres": 646
+    }, {
+        "country": "بهره وردی",
+        "litres": 983
+    }];
+
+    /* Create axes */
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "country";
+
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.axisFills.template.fill = chart.colors.getIndex(2);
+    valueAxis.renderer.axisFills.template.fillOpacity = 0.05;
+
+    /* Create and configure series */
+    var series = chart.series.push(new am4charts.RadarSeries());
+    series.dataFields.valueY = "litres";
+    series.dataFields.categoryX = "country";
+    series.name = "Sales";
+    series.strokeWidth = 3;
+
 });
 //==================
 // Map
@@ -511,8 +788,11 @@ $(function () {
 //==================
 // custom scroll
 //==================
-jQuery(document).ready(function() {
-    $('.scrollbar-inner').scrollbar();
+jQuery(document).ready(function () {
+    if ($('.scrollbar-inner').length) {
+
+        $('.scrollbar-inner').scrollbar();
+    }
 })
 //==================
 // page loader
@@ -534,10 +814,39 @@ $(window).on("load", function () {
 //==================
 // Parallax
 //==================
-$(window).scroll(function(){
-    var self = $(this);
-    var parallax_distance = (self.scrollTop() / 1.5);
-    if(self.scrollTop() > ($("[data-parallax]").offset().top - $("header").outerHeight(true))){
-        $("[data-parallax] .parallax-image").css("background-position-y",parallax_distance + "px");
+$(window).scroll(function () {
+    if ($("[data-parallax]").length) {
+        var self = $(this);
+        var parallax_distance = (self.scrollTop() / 1.5);
+        if (self.scrollTop() > ($("[data-parallax]").offset().top - $("header").outerHeight(true))) {
+            $("[data-parallax] .parallax-image").css("background-position-y", parallax_distance + "px");
+        }
     }
-})
+});
+
+
+//==================
+// sidebar fix
+//==================
+$('[data-sidebarfix]').each(function () {
+    var $this = $(this);
+    $this.sidebarFix({
+        frame: $($this.parents(".landing-content")),
+        topBuffer: 80
+    });
+});
+$(document).on("click", ".landing-sidebar li a", function (e) {
+    $(this).addClass("active").parents("li").siblings("li").find(".active").removeClass("active");
+    if ($($(this).attr("href")).find(".title-landing-item").length) {
+        $("html,body").animate({
+            scrollTop: $($(this).attr("href")).offset().top - ($(this).parents("body").find(".header .slogan").height() + $(this).parents("body").find(".header .bottom-header").height() + 25)
+        }, 500);
+    } else {
+
+        $("html,body").animate({
+            scrollTop: $($(this).attr("href")).offset().top - ($(this).parents("body").find(".header .slogan").height() + $(this).parents("body").find(".header .bottom-header").height())
+        }, 500);
+    }
+    e.stopPropagation();
+    e.preventDefault();
+});
